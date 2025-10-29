f0d8827ff7fee52894cedb99e8c6cba83d46c14d
Taisei Nishiumi <westelh@gmail.com>
2025-10-29 17:01:02 +0900

    Shorten group route placeholders

--- server/src/main/kotlin/com/github/oursharecar/server/routes/GroupRoutes.kt
+++ server/src/main/kotlin/com/github/oursharecar/server/routes/GroupRoutes.kt
@@ -1,7 +1,11 @@
 package com.github.oursharecar.server.routes
 
 import com.github.oursharecar.domain.group.GroupRepository
+import com.github.oursharecar.server.plugins.groups
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.ApplicationCall
 import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.JWTPrincipal
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.coroutines.flow.toList
@@ -14,50 +18,45 @@ fun Route.groupRoutes(groupRepository: GroupRepository) {
         }
         post {
             // TODO: Implement group creation
-            call.respondText("Group creation not implemented", status = io.ktor.http.HttpStatusCode.NotImplemented)
+            call.respond(HttpStatusCode.NotImplemented)
         }
 
-        authenticate("jwt-sub-claims") {
+        authenticate("jwt-groups-claims") {
             get("/{id}") {
+                if (!call.isMemberOfGroup(call.parameters["id"])) {
+                    call.respond(HttpStatusCode.Unauthorized)
+                    return@get
+                }
                 // TODO: Implement get group by ID
-                call.respondText("Get group by ID not implemented", status = io.ktor.http.HttpStatusCode.NotImplemented)
+                call.respond(HttpStatusCode.NotImplemented)
             }
             patch("/{id}") {
                 // TODO: Implement group update
-                call.respondText("Group update not implemented", status = io.ktor.http.HttpStatusCode.NotImplemented)
+                call.respond(HttpStatusCode.NotImplemented)
             }
             delete("/{id}") {
                 // TODO: Implement group deletion
-                call.respondText("Group deletion not implemented", status = io.ktor.http.HttpStatusCode.NotImplemented)
+                call.respond(HttpStatusCode.NotImplemented)
             }
             get("/{id}/members") {
                 // TODO: Implement get group members
-                call.respondText(
-                    "Get group members not implemented",
-                    status = io.ktor.http.HttpStatusCode.NotImplemented
-                )
+                call.respond(HttpStatusCode.NotImplemented)
             }
             post("/{id}/members") {
                 // TODO: Implement add group member
-                call.respondText(
-                    "Add group member not implemented",
-                    status = io.ktor.http.HttpStatusCode.NotImplemented
-                )
+                call.respond(HttpStatusCode.NotImplemented)
             }
             patch("/{id}/members/{memberId}") {
                 // TODO: Implement update group member
-                call.respondText(
-                    "Update group member not implemented",
-                    status = io.ktor.http.HttpStatusCode.NotImplemented
-                )
+                call.respond(HttpStatusCode.NotImplemented)
             }
             delete("/{id}/members/{memberId}") {
                 // TODO: Implement remove group member
-                call.respondText(
-                    "Remove group member not implemented",
-                    status = io.ktor.http.HttpStatusCode.NotImplemented
-                )
+                call.respond(HttpStatusCode.NotImplemented)
             }
         }
     }
 }
+
+fun ApplicationCall.isMemberOfGroup(groupId: String?): Boolean =
+    principal<JWTPrincipal>()?.payload?.groups?.contains(groupId) == true
