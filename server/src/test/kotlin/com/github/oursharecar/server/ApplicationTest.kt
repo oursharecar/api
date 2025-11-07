e8616df355dfb4afcc01b60e8e602741eab732e7
Taisei Nishiumi <westelh@gmail.com>
2025-11-07 22:09:00 +0900

    Remove unused exampleValidToken from ApplicationTest

--- server/src/test/kotlin/com/github/oursharecar/server/ApplicationTest.kt
+++ server/src/test/kotlin/com/github/oursharecar/server/ApplicationTest.kt
@@ -4,9 +4,9 @@ import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldContain
 import io.ktor.client.request.*
-import io.ktor.client.statement.bodyAsText
+import io.ktor.client.statement.*
 import io.ktor.http.*
-import io.ktor.server.config.MapApplicationConfig
+import io.ktor.server.config.*
 import io.ktor.server.testing.*
 
 class ApplicationTest : FunSpec({
@@ -16,8 +16,6 @@ class ApplicationTest : FunSpec({
         "security.jwt.issuer" to "test-issuer",
         "security.jwt.secret" to "a-string-secret-at-least-256-bits-long"
     )
-
-    val exampleValidToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30"
 
     test("GET / returns health status payload") {
         testApplication {
