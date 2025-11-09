7b246ff99547c5e2c9476f6f9dd2c9387f1f04ab
Taisei Nishiumi <westelh@gmail.com>
2025-11-10 00:11:09 +0900

    Add Kover plugin for code coverage support in build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,12 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.ktor) apply false
     alias(libs.plugins.kotlin.plugin.serialization) apply false
+    alias(libs.plugins.kover)
+}
+
+dependencies {
+    kover(project(":core"))
+    kover(project(":server"))
 }
 
 subprojects {
